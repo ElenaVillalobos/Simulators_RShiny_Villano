85056737b05c364d18b6b6c25b4975220fd2e59c
VillaEle <villaele14@gmail.com>
2017-08-16 17:33:10 -0500

    First version of the Trade-off simulator

---
+++ Tradeoff_Simulator/server.R
@@ -0,0 +1,218 @@
+library('shiny')
+library('rsconnect')
+
+
+function(input,output){
+ #######questions########
+ x_ss <- reactive({c(5150,5300,5450,5150,5300,5150,
+                     6050,6200,6350,6050,6200,6050,
+                     5150,5600,6050,5150,5600,5150,
+                     1150,1250,1350,1150,1250,1150)})
+ x_ll <- reactive({c(5300,5450,5600,5450,5600,5600,
+                     6200,6350,6500,6350,6500,6500,
+                     5600,6050,6500,6050,6500,6500,
+                     1250,1350,1450,1350,1450,1450)})
+ t_ss <- reactive({c(1,2,3,1,2,1,
+                     7,8,9,7,8,7,
+                     1,4,7,1,4,1,
+                     1,4,7,1,4,1)})
+ t_ll <- reactive({c(2,3,4,3,4,4,
+                     8,9,10,9,10,10,
+                     4,7,10,7,10,10,
+                     4,7,10,7,10,10)})
+ 
+ #######functions########
+ v_ss <- reactive({1/(input$gamma)*log(1+(input$gamma)*x_ss())})
+ v_ll <- reactive({1/(input$gamma)*log(1+(input$gamma)*x_ll())})
+ w_ss <- reactive({1/(input$tau)*log(1+(input$tau)*t_ss())})
+ w_ll <- reactive({1/(input$tau)*log(1+(input$tau)*t_ll())})
+ Q_time <- reactive({((input$kappa)/(input$alpha))*log(1+(input$alpha)*(((w_ll()-w_ss())/(input$vartheta))^(input$vartheta)))})
+ Q_value <- reactive({(v_ll()-v_ss())})
+ proba_ll <- reactive({(Q_value())/((Q_value())+(Q_time()))})
+ choice <- reactive({round(proba_ll(),1)})
+
+ output$ploti <- renderPlot({ 
+  
+  
+  #######necessary functions for all plots#########
+  
+  tierra <- function(width,position,colorset){
+   x <- position[1]
+   y <- position[2]
+   
+   x1 <- x
+   x2 <- x+width
+   x3 <- x+width
+   x4 <- x
+   
+   y1 <- y
+   y2 <- y
+   y3 <- y+0.25
+   y4 <- y+0.25
+   
+   polygon(x=c(x1,x2,x3,x4),y=c(y1,y2,y3,y4),col=colorset,border=NA)  
+  }
+  olala <- function(proportion,width,variable,colorset){
+   x <- variable[1]
+   y <- variable[2]
+   
+   x1 <- x
+   x2 <- x+proportion*width
+   x3 <- x+proportion*width
+   x4 <- x
+   
+   y1 <- y
+   y2 <- y
+   y3 <- y+0.25
+   y4 <- y+0.25
+   
+   polygon(x=c(x1,x2,x3,x4),y=c(y1,y2,y3,y4),col=colorset,border=NA)
+   text(x4+width+0.25,y2+0.1,paste(proportion),cex=1,col=rgb(10,10,10,maxColor=255, alpha=125)) #proportiontext
+  }
+  ciclo_raw <- function(from,to,dark_color,light_color,medium_color){
+   for(k in from:to){
+    text(x[k],coorY[k]-0.3,labels=paste(k),col=dark_color,cex=size_question)
+    tierra(size_intervals[k],c(coorX[k],coorY[k]),light_color)
+    olala(actual_proportion[k],size_intervals[k],c(coorX[k],coorY[k]),dark_color)
+   }
+  } 
+  eje_canti_gran <- function(distance_line,padj_all,size_outcome,size_attribute,size_letter){
+   plot(0,type='n', xlim=c(0.5,10.8),ylim=c(17,1),axes=F,xlab='',ylab='')
+   axis(3,at=c(1:4,5.5,7:10),
+        labels=c('5150','5300','5450','5600','','6050', '6200','6350', '6500'),
+        tck=0,cex.axis=size_outcome,col='white',padj=padj_all,col.axis=gray[5])
+   axis(3,at=c(1:4,5.5,7:10),
+        labels=c('1','2','3','4','','7', '8','9', '10'),
+        tck=0,cex.axis=size_attribute,col='white',padj=padj_all-1.8,col.axis=gray[5])
+   axis(3,at=c(1:10),labels=(c(LETTERS[1:4],'','',LETTERS[5:8])),
+        tck=0.01,cex.axis=size_letter,padj=padj_all-2.8,lwd=1,col=gray[5],col.axis=gray[2],line=distance_line)
+  }
+  eje_inferior <- function(line_axes){
+   axis(1,at=c(1,4,7,10),labels=c('I','J','K','L'),
+        line=line_axes-0.5,col='white',col.axis=gray[2],padj=-7,cex.axis=axis_size_letters)
+   axis(1,at=c(1,4,7,10),labels=c('1','4','7','10'),
+        line=line_axes-1.6,col='white',col.axis=gray[5],padj=-3,cex.axis=axis_size_attribute)
+   axis(1,at=c(1,4,7,10),labels=c('1150','1250','1350','1450'),
+        line=line_axes,col=gray[5],tck=-0.01,col.axis=gray[5],padj=-4,cex.axis=axis_size_outcome)
+  }
+  
+  
+  pretty_scatter <- function(title_plot,col_axes,
+                              label_x,limit_axis_x,actual_x,
+                              label_y,limit_axis_y,actual_y){
+   plot(0,type='n',xlim=limit_axis_x,ylim=limit_axis_y,xlab='',ylab='',axes=F)
+   mtext(title_plot,3,col=col_axes,line=0)
+   axis(1,col=col_axes,col.axis=col_axes,at=actual_x)
+   mtext(label_x,1,col=col_axes,line=3)
+   axis(2,col=col_axes,col.axis=col_axes,at=actual_y)
+   mtext(label_y,2,col=col_axes,line=3)
+  }
+  pretty_points <- function(x,y,points_color){
+   points(x,y,col=points_color,cex=2,pch=20)
+  }
+  
+  ######especifications###############
+  colores <- list(gray <- c("#6F6F6F37","#6F6F6F64","#6F6F6F96","#6F6F6FC8","#6F6F6FFA"),
+                  set1 <- c("#FFA50046","#FFA50096","#FFA500C8"),
+                  set2 <- c("#9ACD3246","#9ACD3296","#9ACD32C8"),
+                  set3 <- c("#48D1CC46","#48D1CC96","#48D1CCC8"),
+                  set4 <- c("#FF818E46","#FF818E96","#FF818EC8"),
+                  navy <- c("#1594C546","#1594C596","#1594C5C8"),
+                  blue <- c("#79C3F146","#79C3F196","#79C3F1C8"),
+                  pur1 <- c("#D07FD819","#D07FD87D","#D07FD8FA"),
+                  pur2 <- c("#7571FE19","#7571FE7D","#7571FEFA"))
+  size_intervals <- c(1,1,1,2,2,3,
+                      1,1,1,2,2,3,
+                      3,3,3,6,6,9,
+                      3,3,3,6,6,9)
+  coorX <- c(1,2,3,1,2,1,
+             7,8,9,7,8,7,
+             1,4,7,1,4,1,
+             1,4,7,1,4,1)
+  coorY <- c(1,2,1,3,4,5,
+             1,2,1,3,4,5,
+             6,7,6,8,9,10,
+             13,14,13,15,16,17)  
+  x <- c(1.5,2.5,3.5,2,3,2.5,
+         7.5,8.5,9.5,8,9,8.5,
+         2.5,5.5,8.5,3.5,7.5,5.5,
+         2.5,5.5,8.5,4,7,5.5)
+ 
+  axis_size_outcome <- 1
+  axis_size_letters <- 0.9
+  axis_size_attribute <- 1
+  size_question <- 1.2
+  actual_proportion <- choice()
+  
+  
+  
+  
+  
+  
+  ##########actual plots############
+  layout(matrix(c(1,1,2,2,6,6,
+                  1,1,2,2,6,6,
+                  3,3,4,4,6,6,
+                  3,3,4,4,6,6,
+                  0,5,5,0,6,6,
+                  0,5,5,0,6,6),ncol=6,byrow=T))
+  
+  pretty_scatter('Value Function',gray[5],
+                 'Magnitude',c(1000,7000),seq(1000,7000,1000),
+                 'Value',c(0,7000),seq(0,7000,2000))
+  pretty_points(x_ss(),v_ss(),navy[1])
+  pretty_points(x_ll(),v_ll(),navy[1])
+  
+  pretty_scatter('Weight Function',gray[5],
+                 'Time',c(1,10),seq(1,10,1),
+                 'Weight',c(0,10),seq(1,10,1))
+  pretty_points(t_ss(),w_ss(),navy[1])
+  pretty_points(t_ll(),w_ll(),navy[1])
+  
+  pretty_scatter('Differences in Value',gray[5],
+                 'Difference of each question',c(1,24),seq(1,23,2),
+                 'Value of Difference',c(0,200),seq(0,200,40))
+  pretty_points(c(1:6),Q_value()[c(1:6)],set1[3])
+  pretty_points(c(7:12),Q_value()[c(7:12)],set2[3])
+  pretty_points(c(13:18),Q_value()[c(13:18)],set3[3])
+  pretty_points(c(19:24),Q_value()[c(19:24)],set4[3])
+  
+  pretty_scatter('Tradeoff Function',gray[5],
+                 'Difference of each question',c(1,24),seq(1,23,2),
+                 'Value of Difference',c(0,10),seq(0,10,2))
+  pretty_points(c(1:6),Q_time()[c(1:6)],set1[3])
+  pretty_points(c(7:12),Q_time()[c(7:12)],set2[3])
+  pretty_points(c(13:18),Q_time()[c(13:18)],set3[3])
+  pretty_points(c(19:24),Q_time()[c(19:24)],set4[3])
+  
+  pretty_scatter('Choice probability',gray[5],
+                 'Question',c(1,24),seq(1,23,2),
+                 'P(LL)',c(0,1),seq(0,1,0.2))
+  pretty_points(c(1:6),proba_ll()[c(1:6)],set1[3])
+  pretty_points(c(7:12),proba_ll()[c(7:12)],set2[3])
+  pretty_points(c(13:18),proba_ll()[c(13:18)],set3[3])
+  pretty_points(c(19:24),proba_ll()[c(19:24)],set4[3])
+  
+  ######plot tiempo
+  par(mar=c(5,1,6,0.5))
+  eje_canti_gran(0.5,0.5,axis_size_outcome,axis_size_attribute,axis_size_letters)
+  eje_inferior(-17.3)
+  mtext('Probability of choosing the LL alternative',3,col=gray[5],line=4.5)
+  ciclo_raw(1,6,set1[3],set1[1],set1[2])   
+  ciclo_raw(7,12,set2[3],set2[1],set2[2])   
+  ciclo_raw(13,18,set3[3],set3[1],set3[2])   
+  ciclo_raw(19,24,set4[3],set4[1],set4[2])   
+  
+ })
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ Tradeoff_Simulator/ui.R
@@ -0,0 +1,123 @@
+
+library('shiny')
+library('rsconnect')
+
+
+fluidPage(title='Trade-off Model',
+ tabsetPanel(
+  tabPanel(title='Simulator',
+           
+          titlePanel('Simulator for the Trade-off Model'),
+          h6('See the Instructions tab above',style="color: #6F6F6F"),
+          fluidRow(
+                   column(3,h4('by Elena Villalobos Nolasco',style="color: #6F6F6F")),
+                   column(3,h5('E-mail:',a(href='villaele14@gmail.com','villaele14@gmail.com'),style="color: #6F6F6F")),
+                   column(3,h5('Git:',a(href='https://github.com/VillanoEle','github.com/VillanoEle'),style="color: #6F6F6F")),
+                   column(3,h5('Webpage:',a(href='https://sites.google.com/site/adaptabilidad25/home','Lab25.'),style="color: #6F6F6F"))),
+   fluidRow(
+   column(3,
+ wellPanel(
+          sliderInput(inputId= "tau",label = p("Weight function (", em('\u03C4'), ')') ,
+                      value = 0.001, min=0.001,max=5,sep=0.01),
+          sliderInput(inputId= "gamma",label = p("Value Function (", em('\u03B3'), ')') ,
+                      value = 0.03,min=0.001,max=5,sep=0.01),
+          sliderInput(inputId= "kappa",label = p("Tradeoff function (", em('\u03BA'), ')') ,
+                      value = 2.5,min=0.01,max=5,sep=0.01),
+          sliderInput(inputId= "vartheta",label = p("Tradeoff function/Superadditivity (", em('\u03D1'), ')') ,
+                      value = 2.5, min=1.01, max=10,sep=0.01),
+          sliderInput( inputId= "alpha",label = p("Tradeoff function/Subadditivity (", em('\u03B1'), ')') ,
+                       value = 5,min=0.01,max=10,sep=0.01))),
+   column(8,  plotOutput("ploti",width = "900px",height="600px")))),
+ tabPanel(title='Instructions',
+          titlePanel('Instructions for the Trade-off Simulator'),
+          p('This shinyapp was created to show how the Trade-off Model of',
+            a(href='http://onlinelibrary.wiley.com/doi/10.1111/cogs.12104/epdf',
+                  'Scholten, Read & Sanborn (2014)'), 'works.'),
+          h3('Structure'),
+          p(strong('1.'), 'The mathematical model is fully displayed in the Equations tab.'),
+          p(strong('2.'), 'The Simulator tab is divided in three main parts:'),
+          p(strong('2.1.'), 'The left side shows the parameters that you might want to play with.'),
+          p(strong('2.2.'), 'The central part has five plots, each one is representing a function 
+            from the model.'),
+          p(strong('2.3.'), 'The right part is a graphic that represents the choice probability 
+            for the larger later reward given certain parameter values. The experimental design
+            is based on the second study of Scholten et al (2014). The graphic consists in 
+            12 fixed alternatives. The combinations of these created 24 questions which are 
+            classified in 4 sets, 6 questions each:',
+            
+            tags$ol(
+            tags$li('Short Delays',style="color: #FFA500"),
+            tags$li('Long Delays',style="color: #9ACD32"),
+            tags$li('Long intervals',style="color: #48D1CC"),
+            tags$li('Small outcomes, long intervals',style='color: #FF818E')), 
+           
+            'In this graphic, each bar represents a question, the leftmost end indicates
+            the smaller reward, and the rightmost end the larger reward. The alternatives
+            are indicated in the superior axis (noting that for the fourth set there are 
+            small outcomes). The darkness of the bar represents the choice probability for 
+            the larger later reward given certain parameter values. For example, if the 
+            model predicts that the larger reward might be chosen with 0.8 of probability, 
+            the the bar will be darker 8/10.'),
+          p(strong('3.'),'I recommend you to check specifically the following parameter 
+            combinations in order to observe empirical phenomena:',
+            
+            tags$ul(
+             tags$li('Superadditivity:',  em('\u03C4'), '= 0.001,', em('\u03B3'), '= 2,   ',em('\u03BA'),'= 2,   ', em('\u03D1'), '= 5.5, ',em('\u03B1'), '= 0.01.'),
+             tags$li('Subadditivity:',    em('\u03C4'), '= 0.001,', em('\u03B3'), '= 0.1, ',em('\u03BA'),'= 1.26,', em('\u03D1'), '= 1.1, ',em('\u03B1'), '= 7.'),
+             tags$li('Magnitude Effect:', em('\u03C4'), '= 2.5,  ', em('\u03B3'), '= 0.2, ',em('\u03BA'),'= 0.16,', em('\u03D1'), '= 1.01,',em('\u03B1'), '= 0.1.'),
+             tags$li('Immediacy Effect:', em('\u03C4'), '= 0.14, ', em('\u03B3'), '= 1.15,',em('\u03BA'),'= 3,   ', em('\u03D1'), '= 1.1, ',em('\u03B1'), '= 0.1.'))),
+          
+          h3('Some background'),
+            p(strong('Superaddivity:'), 'Less discounting over an undivided interval than over 
+            a subdivided one, i.e. to choose the larger later reward over subintervals
+            but the smaller sooner over undivided intervals.'),
+            p(strong('Subadditivity:'), 'More discounting over an undivided interval than over 
+            a subdivided one, i.e. to choose the larger later reward over undivided intervals
+            but the smaller sooner over subintervals.'),
+            p(strong('Magnitude effect:'), 'Smaller outcomes discounted at a higher rate than
+            larger ones, i.e. to choose the larger later rewards when the alternatives
+            are too large.'),
+            p(strong('Immediacy effect:'), 'Overweighting of immediate consump,
+            i.e. to choose the immediate but smaller reward over a larger but later reward.')
+           
+          ),
+            
+         #como le das poco peso a los intervalos eliges la grande demorada
+ tabPanel(title='Equations',
+          titlePanel('Equations'),
+          
+  wellPanel(
+  fluidRow(column(2),column(4,withMathJax('Value function: 
+                                 $$v(x_{s,l})=\\frac{1}{\\gamma}\\cdot\\log(1+\\gamma\\cdot x_{s,l})$$')),
+           column(1),
+           column(3,p(em('\u03B3'), '= Diminishing absolute sensitivity to outcomes;'),
+                    p(em('x'),'= Outcome [',em('s'), 'smaller,', em('l'), 'larger ].'))),
+  fluidRow(column(2),column(4,withMathJax('Weight function: 
+                                 $$w(t_{s,l})=\\frac{1}{\\tau}\\cdot\\log(1+\\tau\\cdot t_{s,l})$$')),
+           column(1),
+           column(3,p(em('\u03C4'), '= Diminishing absolute sensitivity to delays;'),
+                  p(em('t'),'= Delay [', em('s'), 'sooner,', em('l'), 'later ].'))),
+  fluidRow(column(2),column(4,withMathJax('Difference in values: 
+                                 $$A(x_s,x_l)=v(x_l)-v(x_s)$$'))),
+  fluidRow(column(2),column(4,withMathJax('Trade-off function: 
+                                 $$Q(t_s,t_l)=\\frac{\\kappa}{\\alpha}\\cdot\\log 
+                                 \\left( 1+\\alpha\\cdot \\left(\\frac{w(t_l)-w(t_s)}{\\vartheta}\\right)
+                                 ^\\vartheta \\right)$$')),
+           column(1),
+           column(3,p(em('\u03BA'), '= Time sensitivity;'),
+                    p(em('\u03D1'), '= Superadditivity;'),
+                    p(em('\u03B1'), '= Subadditivity.'))),
+  fluidRow(column(2),column(4,withMathJax('Probability assigment for the larger later alternative:
+                                 $$P(ll)=\\frac{A(x_s,x_l)}{A(x_s,x_l)-Q(t_s,t_l)}$$')))
+
+  ))))
+
+
+
+
+
+
+
+
+
+
