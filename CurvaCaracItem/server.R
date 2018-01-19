d5158330fd61def0dd56b13ada85bcd0a17ca59b
VillaEle <villaele14@gmail.com>
2018-01-19 14:27:47 -0600

    Updates

--- CurvaCaracItem/server.R
+++ CurvaCaracItem/server.R
@@ -3,16 +3,22 @@
 server <- function(input,output){
  theta <- reactive({seq(-10,10,1)})
  proba_theta <- reactive({input$ce+(1-input$ce)*(1/(1+exp(-input$alpha*(theta()-input$beta))))})
- output$grafico <- renderPlot({
+ proba_theta_2 <- reactive({input$ce_2+(1-input$ce_2)*(1/(1+exp(-input$alpha_2*(theta()-input$beta_2))))})
+ 
+
+  output$grafico <- renderPlot({
   layout(matrix(c(0,1,1,0),ncol=4))
   par(mar=c(5,2,4,0))
   plot(0, type='n',xlim=c(-10,10),ylim=c(0,1),xlab='',ylab='',axes=F)
   mtext(expression(theta),1, line=4,col='gray30',cex=2)
   mtext(expression(f(theta)),2,line=3.5,col='gray30',cex=2,las=2)
+  legend(5,0.7,col=c('turquoise3','tomato'),pch=20,legend=c('Item 1','Item 2'),
+         cex=2,box.col='white',text.col='gray30')
   axis(1, at=c(-10:10),cex.axis=1.2,col='gray30',col.axis='gray30')
   axis(2,las=2,cex.axis=1.3,col='gray30',col.axis='gray30')
   points(theta(),proba_theta(),type='b',col='turquoise3',pch=16,cex=1.2,lwd=1.8)
-
+  points(theta(),proba_theta_2(),type='b',col='tomato',pch=16,cex=1.2,lwd=1.8)
+  
 
  })
  
--- CurvaCaracItem/ui.R
+++ CurvaCaracItem/ui.R
@@ -1,28 +1,37 @@
 
 
 fluidPage(
- tabsetPanel(
-  tabPanel('Gráficos',
-           h1('Curva Característica del Item CCI',style="color: #6F6F6F"),
-           h5('Por Elena Villalobos',style="color: #6F6F6F"),
+
+           h2('Curva Característica del Item CCI',style="color: #6F6F6F"),
            hr(),
+           withMathJax('$$f_j(\\theta_i)=c_j+(1-c_j)\\frac{1}{1+e ^ {-a_j(\\theta_i-b_j)}}$$'),
            plotOutput('grafico'),
            br(),
            fluidRow(
             column(4,
-                   h4('Dificultad'),
-                   sliderInput(inputId='beta',label='b',
-                               min=0,max=10,value=0)),
+                   h4('Dificultad (b)'),
+                   sliderInput(inputId='beta',label='Item 1 (b)',
+                               min=0,max=10,value=0),
+                   sliderInput(inputId='beta_2',label='Item 2 (b)',
+                               min=0,max=10,value=2)
+                   ),
             column(4,
-                   h4('Discriminación'),
-                   sliderInput(inputId='alpha',label='a',
+                   h4('Discriminación (a)'),
+                   sliderInput(inputId='alpha',label='Item 1 (a)',
+                               min=-5,max=5,value=1),
+                   sliderInput(inputId='alpha_2',label='Item 2 (a)',
                                min=-5,max=5,value=1)),
             column(4,
-                   h4('Adivinación'),
-                   sliderInput(inputId='ce',label='c',
+                   h4('Adivinación (c)'),
+                   sliderInput(inputId='ce',label='Item 1 (c)',
+                               min=0,max=1,value=0),
+                   sliderInput(inputId='ce_2',label='Item 2 (c)',
                                min=0,max=1,value=0))
-                           )
-           )
-       )
- )
+
+       ),
+       hr(),
+       fluidRow(#column(2,offset=4,h6('Por Elena Villalobos',style="color: #6F6F6F")),
+                column(2,offset=5,h6(a(href='https://github.com/VillanoEle','github.com/VillanoEle'),style="color: #6F6F6F"))))
+       
+
  
